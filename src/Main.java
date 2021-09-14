d148444ce06c753a76d477e13d09c6303c5e560a
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2021-09-13 20:32:47 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main
     public static void main(String[] args)
     {
         //test
-        //stuff
+        //final
         Scanner reader = new Scanner(System.in);
         String buffer;
 
